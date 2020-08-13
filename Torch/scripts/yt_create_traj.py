877ec0564f5516dc7e338fe4c4b832747ea8bef2
Robert Fisher <robert.fisher@umassd.edu>
2020-08-13 06:39:13 -0400

    Added comments for review.

--- Torch/scripts/yt_create_traj.py
+++ Torch/scripts/yt_create_traj.py
@@ -1,9 +1,12 @@
 import yt
 import glob
 
+# Need some comments here to describe what this script does. What is it inputting? What does it do? How does it work?
+
 f_parts = glob.glob("/work/07441/kbhargav/stampede2/puredet2/pure_det_hdf5_part_*")
 f_parts.sort()
 
+# Is Y_e stored in both approx19 and parameteric burn? I'm sure it is in the latter.
 fields = ["particle_dens", "particle_temp", "particle_ye", "particle_velocity_x", "particle_velocity_y"]
 
 ds = yt.load(f_parts[0])
