06979c42df837c53e869ff2fd6db2a6da04d0ee0
Robert Fisher <robert.fisher@umassd.edu>
2020-08-13 06:34:54 -0400

    Added comments for review.

--- Torch/scripts/create_traj.py
+++ Torch/scripts/create_traj.py
@@ -2,11 +2,17 @@ import numpy as np
 import h5py
 
 nfiles = 4839
-nparticles = 10002
+nparticles = 10002  # Can you get this from the first data file without
+                    #  hardcoding it?
 
 time = np.zeros([nparticles,nfiles], dtype = float)
 temp = np.zeros([nparticles,nfiles], dtype = float)
 dens = np.zeros([nparticles,nfiles], dtype = float)
+
+#rtf Instead of doing a loop, why not do a glob? This will both simplify
+#   the code. Further, I can envision cases where a glob will work
+#   and this code will not -- for example, if one has only every N
+#   files for a quick analysis
 
 for f in range(nfiles):
     if(f <10):
@@ -25,11 +31,15 @@ for f in range(nfiles):
     array2 = np.asarray(p_file['real scalars'])
     for i in range(nparticles):
         ind = int(array1[11][i]) - 1
+#rtf Need some comments here to clarify precisely how you are 
+#     extracting the data.
         time[ind][f] = float(array2[1][1])
         dens[ind][f] = float(array1[1][i])
         temp[ind][f] = float(array1[12][i])
     print("Reading file " + part + "has completed...")
     p_file.close()
+
+# Don't we need velocity information for SuperNu?
 
 for p in range(nparticles):
     history = "tempdens" + str(p+1) + ".dat"
