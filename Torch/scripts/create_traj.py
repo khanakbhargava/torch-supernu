2aee4e7ab93ec90870727d5b2152792210c0b91b
Khanak Bhargava <kbhargav@login1.stampede2.tacc.utexas.edu>
2020-08-15 15:46:39 -0500

    Changes to create_traj.py

--- Torch/scripts/create_traj.py
+++ Torch/scripts/create_traj.py
@@ -1,47 +1,41 @@
-import numpy as np
+### This script will read data from FLASH particle files and create '.dat' trajectory files for Torch input
+
 import h5py
+import glob
+import numpy as np
 
-nfiles = 4839
-nparticles = 10002  # Can you get this from the first data file without
-                    #  hardcoding it?
+#Give path to particle files
+files = glob.glob("/work/07441/kbhargav/stampede2/puredet2/pure_det_hdf5_part_00[0-4][0-9][0-9][0-9]")
+files.sort()
 
+#Getting total number of particles from the first file
+p_file = h5py.File(files[0],'r')
+dset = p_file['tracer particles']
+array1 = np.transpose(dset)
+nparticles = array1.shape[1]
+
+#Getting total number of files
+nfiles = len(files)
+
+#Creating arrays to store data
 time = np.zeros([nparticles,nfiles], dtype = float)
 temp = np.zeros([nparticles,nfiles], dtype = float)
 dens = np.zeros([nparticles,nfiles], dtype = float)
 
-#rtf Instead of doing a loop, why not do a glob? This will both simplify
-#   the code. Further, I can envision cases where a glob will work
-#   and this code will not -- for example, if one has only every N
-#   files for a quick analysis
+#Reading data from particle files
+for f,file in enumerate(files):
+	p_file = h5py.File(file,'r')
+	dset = p_file['tracer particles']
+	array1 = np.transpose(dset)
+	array2 = np.asarray(p_file['real scalars'])
+	for i in range(nparticles):
+        	ind = int(array1[11][i]) - 1
+        	time[ind][f] = float(array2[1][1])
+        	dens[ind][f] = float(array1[1][i])
+        	temp[ind][f] = float(array1[12][i])
+	p_file.close()
 
-for f in range(nfiles):
-    if(f <10):
-        part = "pure_det_hdf5_part_00000" + str(f)
-    elif(f >= 10) and (f<= 99):
-        part = "pure_det_hdf5_part_0000" + str(f)
-    elif(f >= 100) and (f<= 999):
-        part = "pure_det_hdf5_part_000" + str(f)
-    elif(f >= 1000):
-        part = "pure_det_hdf5_part_00" + str(f)
-    p_file = h5py.File(part,'r')
-    dset = p_file['tracer particles']
-    array1 = np.transpose(dset)
-    #dset = p_file['real scalars']
-    #array2 = dset[()]
-    array2 = np.asarray(p_file['real scalars'])
-    for i in range(nparticles):
-        ind = int(array1[11][i]) - 1
-#rtf Need some comments here to clarify precisely how you are 
-#     extracting the data.
-        time[ind][f] = float(array2[1][1])
-        dens[ind][f] = float(array1[1][i])
-        temp[ind][f] = float(array1[12][i])
-    print("Reading file " + part + "has completed...")
-    p_file.close()
-
-# Don't we need velocity information for SuperNu?
-
+#Creating .dat trajectory files for Torch
 for p in range(nparticles):
-    history = "tempdens" + str(p+1) + ".dat"
-    np.savetxt(history,np.c_[time[p],temp[p],dens[p]], fmt='%1.5e')
-    print("History for particle # "+str(p+1)+" written...")
+	history = "tempdens" + str(p+1) + ".dat"
+	np.savetxt(history,np.c_[time[p],temp[p],dens[p]], fmt='%1.5e')
