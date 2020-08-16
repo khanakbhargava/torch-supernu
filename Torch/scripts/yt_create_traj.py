f7dc3c4a0da35610c495816c1c4ca44c861748e2
Khanak Bhargava <kbhargav@login2.stampede2.tacc.utexas.edu>
2020-08-16 16:47:31 -0500

    Changes to yt_create_traj.py

--- Torch/scripts/yt_create_traj.py
+++ Torch/scripts/yt_create_traj.py
@@ -1,18 +1,21 @@
+### This script uses yt to create particle trajectories from FLASH particle files
+
 import yt
 import glob
 
-# Need some comments here to describe what this script does. What is it inputting? What does it do? How does it work?
-
+#Give path to particle files
 f_parts = glob.glob("/work/07441/kbhargav/stampede2/puredet2/pure_det_hdf5_part_*")
 f_parts.sort()
 
-# Is Y_e stored in both approx19 and parameteric burn? I'm sure it is in the latter.
-fields = ["particle_dens", "particle_temp", "particle_ye", "particle_velocity_x", "particle_velocity_y"]
+#Give fields to be included in the trajectories. Positions are added by default.
+fields = ["particle_dens", "particle_temp", "particle_velocity_x", "particle_velocity_y"]
 
+#Get indices of all available  particles
 ds = yt.load(f_parts[0])
 dd = ds.all_data()
 indices = dd["particle_index"].astype("int64")
 
+#Create individual '.dat' files for every particle
 ts = yt.DatasetSeries(f_parts)
 trajs = ts.particle_trajectories(indices, fields=fields) 
 trajs.write_out("traj")
