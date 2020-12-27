66997c3da47e655fd46328cc72dcf79cbe19f29c
Khanak Bhargava <kbhargav@login3.stampede2.tacc.utexas.edu>
2020-12-27 12:42:53 -0600

    Added new generalised trajectory creation script.

--- Torch/scripts/traj.py
+++ Torch/scripts/traj.py
@@ -1,63 +1,59 @@
-## This is a generalized script that can also create trajectories for models that lose particles
-
 import h5py
 import glob
+import numpy as np
 
-files = glob.glob("/scratch/07441/kbhargav/hot_en_traj/particle_data_rot_hot_envelope/rot_flame_det_4km_hdf5_part_0[0-1][0-9][0-9][0-9][0-9]")
+#Give path to particle files
+files = glob.glob("/home/kbhargava/ddt_off_imp_files_stampede_noperturbation/ddt_off_hdf5_part_*")
 files.sort()
 
-#print(len(files))
-
-time = []
-temp = []
-dens = []
-
 indices = []
+miss = []
+print(len(files))
 
-file = h5py.File(files[18316],"r")
+file = h5py.File(files[-1],"r")
+print(file)
 row1, column1 = file["tracer particles"].shape
 for j in range(row1):
-    indices.append(int(file["tracer particles"][j][11]))
+        indices.append(int(file["tracer particles"][j][11]))
 
-#print(row1,column1)
-#print(indices)
+for j in range(9997):
+        if j in indices:
+                continue
+        else:
+                miss.append(j+1)
 
-for i in range(len(files)):
-    time.append({0:"Time%s"%(i)})
-    temp.append({0:"Temp%s"%(i)})
-    dens.append({0:"Density%s"%(i)})
-    file = h5py.File(files[i],"r")
-    row, column = file["tracer particles"].shape
-    for j in range(row):
-         ptag = int(file["tracer particles"][j][11])
-         if (ptag in indices):
-              time[i].update({ptag : str(file["real scalars"][1][1])})
-              temp[i].update({ptag : str(file["tracer particles"][j][12])})
-              dens[i].update({ptag : str(file["tracer particles"][j][1])})
+print('Got the missing indices', miss)
 
-#print(str(time[0][indices[i]]))
+print('indices len:',len(indices))
+nfiles = len(files)
+nparticles = 9997
 
-#for i in range(len(files)):
-#for ptag  in indices:
-#    ptag = indices[j]
-#    history = "tempdens" + str(ptag) + ".dat"
-#    print(history)
-#    with open(history,'a') as f: 
-         #for j in range(len(files)):
-         #     !time1 = float(time[j][ptag])
-         #     !temp1 = float(temp[j][ptag])
-         #     !dens1 = float(dens[j][ptag])
-#         f.write('%e %e %e' %(time[i][ptag], temp[i][ptag], dens[i][ptag]))
-#         f.write('\n')
+print('Creating arrays to store data')
+time = np.zeros([nparticles,nfiles], dtype = float)
+temp = np.zeros([nparticles,nfiles], dtype = float)
+dens = np.zeros([nparticles,nfiles], dtype = float)
 
-for ptag in indices:
-    history = "tempdens" + str(ptag) + ".dat"
-    with open(history, 'w') as f:
-         for i in range(len(files)):
-              #f.write(str(time[i][ptag]) + '\t\t\t' + str(temp[i][ptag]) + '\t\t\t' + str(dens[i][ptag]))
-              time1 = float(time[i][ptag])
-              temp1 = float(temp[i][ptag])
-              dens1 = float(dens[i][ptag])
-              #f.write('%s %s %s' %(time[i][ptag], temp[i][ptag], dens[i][ptag]))
-              f.write('%e %e %e' %(time1, temp1, dens1))
-              f.write('\n')
+print('#Reading data from particle files')
+for f,file in enumerate(files):
+        p_file = h5py.File(file,'r')
+        dset = p_file['tracer particles']
+        array1 = np.transpose(dset)
+        array2 = np.asarray(p_file['real scalars'])
+        column2 = array1.shape[1]
+#       print(column2)
+        for i in range(column2):
+                ind = int(array1[11][i]) - 1
+                if (ind in miss):
+                        continue
+                else:
+                        time[ind][f] = float(array2[1][1])
+                        dens[ind][f] = float(array1[1][i])
+                        temp[ind][f] = float(array1[12][i])
+        #print('Finished reading' + str(f+1) + 'file')
+        p_file.close()
+
+print('Creating .dat trajectory files for Torch')
+
+for p in indices:
+        history = "tempdens" + str(p) + ".dat"
+        np.savetxt(history,np.c_[time[p-1],temp[p-1],dens[p-1]], fmt='%1.5e')
